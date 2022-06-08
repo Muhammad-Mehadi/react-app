7a3dd29b8667d55195322e88e150a6326b7f355b
mehadi <mehadi.contact@gmail.com>
2022-06-08 14:50:14 +0600

    react-condition

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import Details from "./components/Details";
+
 function App() {
   return (
     <div className="App">
-      <Details name={"Muhammad Mehadi"} age={"25"} />
-      <Details name={"Samrat Akbar"} age={"32"} />
+      <div>
+        <Details name={"Muhammad Mehadi"} age={"25"} />
+        <Details name={"Samrat Akbar"} age={"32"} />
+      </div>
     </div>
   );
 }
--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './details.css';
 const Details = (props) => {
+ var name= props.name;
   return (
-    <div className='details-section'>
-      <h1>{props.name}</h1>
-      <h3>{props.age} years old.</h3>
-    </div>
+     <>
+    {name === "Muhammad Mehadi" ? <div className='details-section'>
+    <h1>{props.name}</h1>
+    <h3>{props.age} years old.</h3>
+  </div> : <div></div>}
+  </>
   )
 }
 
