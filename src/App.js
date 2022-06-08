30a90c3686542190a7e9d1c88827276b1ed9e6af
mehadi <mehadi.contact@gmail.com>
2022-06-08 15:42:36 +0600

    Route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Form from "./components/Form";
-
+import Blog from "./components/Blog/Blog";
+import Blogs from "./components/Blog/Blogs";
+import Contact from "./components/Blog/Contact";
+import Error from "./components/Blog/Error";
+import Index from "./components/Blog/Index";
 function App() {
   return (
     <div className="App">
-      <Form />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />}></Route>
+          <Route path="/blogs" element={<Blogs />}></Route>
+          <Route path="/blogs/:id" element={<Blog />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/*" element={<Error />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
